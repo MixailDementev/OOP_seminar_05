35a92cf60a65fea87f4f8258b375a4489296d9fe
MDementev <vanneste1207@gmail.com>
2023-02-03 15:27:06 +0300

    DZ5

---
+++ Main.java
@@ -0,0 +1,16 @@
+// 333package OOP_seminar_05;
+
+public class Main {
+    public static void main(String[] args) {
+        // Filemanager run = new Filemanager();
+        // Addressbook phones = new Addressbook();
+        // phones.AddressBook();
+        // run.ExportFile(phones.pb);
+        // run.ImportFile(phones.pb);
+        // phones.showAdressBook();
+
+        Menu.mainMenu();
+    }
+}
+
+
